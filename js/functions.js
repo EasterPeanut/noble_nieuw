58b3f0f35675eb6191bc11dddabd5c4834758358
Tom Arnoldussen <tom.arnoldussen@gmail.com>
2014-06-05 14:10:41 +0200

    Bij alle clicks reload

--- js/functions.js
+++ js/functions.js
@@ -35,3 +35,60 @@ $(".ui-content").height(content);
 
 var id = 1;
 
+
+function loadSentMessages(){
+	$( ".page3-list" ).empty();
+	 $.ajax({ 
+                type: 'GET', 
+                url: "getmessages.php?id="+id+"", 
+                dataType: 'json',
+                success: function (data) { 
+                   
+                    $.each(data, function(index, element) {
+                        console.log(element[1].date);
+                        for (var key in element) {
+                           var obj = element[key];
+                           for (var prop in obj) {
+                            if((prop == "id_from") && (obj['id_from'] == id)) {
+                                 $('.page3-list').append('<li><a href="#" class="ui-btn" data-transition="slide"><ul><li>'+obj.message+'</li><li>'+obj.date+ '</li></ul></a></li>');
+              
+                            }
+                             
+                           }
+                        }
+                        
+                    });
+                }
+            });
+}
+function loadReceivedMessages(){
+	$( ".page2-list" ).empty();
+ $.ajax({ 
+                type: 'GET', 
+                url: "getmessages.php?id="+id+"", 
+                dataType: 'json',
+                success: function (data) { 
+                   
+                    $.each(data, function(index, element) {
+                        console.log(element[1].date);
+                        for (var key in element) {
+                           var obj = element[key];
+                           for (var prop in obj) {
+                            if((prop == "id_to") && (obj['id_to'] == id)) {
+                                 $('.page2-list').append('<li><a href="#" class="ui-btn" data-transition="slide"><ul><li>'+obj.message+'</li><li>'+obj.date+ '</li></ul></a></li>');
+              
+                            }
+                             
+                           }
+                        }
+                        
+                    });
+                }
+            });
+}
+
+$(document).on('click touchstart','a', function () {
+	loadSentMessages();
+	loadReceivedMessages();
+	console.log("haaaa");
+	 });
