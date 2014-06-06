bbcdfae53282515f42372018bd0b0d2fff0a3790
Tom Arnoldussen <tom.arnoldussen@gmail.com>
2014-06-06 20:18:29 +0200

    Locatie toegevoegd

--- updateuser.php
+++ updateuser.php
@@ -3,10 +3,19 @@ $id = $_GET["id"];
 $lat = $_GET["lat"];
 $lon = $_GET["lon"];
 $date = date('Y-m-d H:i:s');
-/*http://localhost/save/noble/updateuser.php?id=1&lat=51.451990&lon=5.480222*/
+$url = 'http://maps.googleapis.com/maps/api/geocode/json?latlng='.$lat.','.$lon.'&sensor=true';
 
-	 mysqli_query($con,"UPDATE user SET location_lat=$lat, location_lon=$lon, date='".$date."'
-WHERE userid=$id");
+/*http://localhost/save/noble/updateuser.php?id=1&lat=51.451990&lon=5.480222*/
+$json = file_get_contents($url);
+$obj = json_decode($json);
+
+$city = $obj->{'results'}[0]->{'address_components'}[4]->{'long_name'};
+$street = $obj->{'results'}[0]->{'address_components'}[1]->{'long_name'};
+$location_name = $city .", ".$street;
+$query = "UPDATE user SET location_lat=$lat, location_lon=$lon, date='".$date."', location_name='$location_name'
+WHERE userid=$id";
+
+	 mysqli_query($con,$query);
 	 mysqli_close($con);
 
 ?>
